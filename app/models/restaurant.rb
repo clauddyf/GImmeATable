192b68f89227da2d8f858d87a9613a996ab6703f
claudius <claudius.solomon@gmail.com>
2019-12-30 18:06:53 -0800

    first search troubleshooting

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,21 +4,21 @@ class Restaurant < ApplicationRecord
     belongs_to :location, class_name: :Location, foreign_key: :city_id
     has_many :reviews
     has_many_attached :photos
-
-
+    
+    # debugger
     def self.search_by_city(query)
-        param = '%' + query.downcase + '%'
-        Restaurant.where('lower(city) LIKE ?', param).limit(10)
-      end
+      param = '%' + query.downcase + '%'
+      Location.where('lower(cityname) LIKE ?', param).limit(10)
+    end
     
-      def self.search_by_cuisine(query)
-        param = '%' + query.downcase + '%'
-        Restaurant.where('lower(cuisine) LIKE ?', param).limit(10)
-      end
+    def self.search_by_cuisine(query)
+      param = '%' + query.downcase + '%'
+      Restaurant.where('lower(cuisine_type) LIKE ?', param).limit(10)
+    end
     
-      def self.search_results(query)
-        param = '%' + query.downcase + '%'
-        by_name = Restaurant.where('lower(name) LIKE ?', param).limit(10)
-        by_name + search_by_city(query) + search_by_cuisine(query)
-      end
+    def self.search_results(query)
+      param = '%' + query.downcase + '%'
+      by_name = Restaurant.where('lower(name) LIKE ?', param).limit(10)
+      by_name + search_by_city(query) + search_by_cuisine(query)
+    end
 end
