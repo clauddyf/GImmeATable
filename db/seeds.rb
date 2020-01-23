725523507c2144dd175b801d681932ffc597f1d1
claudius <claudius.solomon@gmail.com>
2020-01-23 10:26:06 -0800

    the seed for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,8 @@ require 'csv'
 
 
 Location.destroy_all
-# Restaurant.destroy_all
+Restaurant.destroy_all
+User.destroy_all
 
 
 
@@ -38,6 +39,40 @@ Location.all.each_with_index do |location, idx|
     location.photos.attach(io: file, filename: "locshow_#{idx}.jpg")
     puts "#{location.cityname} second photos have been saved"
 end
+csv_text = File.read(Rails.root.join('db','lib','finalSeed.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+    t = Restaurant.new
+    
+    t.name = row['name']
+    t.description = row['description']
+    t.hours_op = row['hours_op']
+    t.dresscode = row['dresscode']
+    t.address = row['address']
+    t.neighborhood = row['neighborhood']
+    t.website = row['website']
+    t.city_id = row['city_id']
+    t.phone_num= row['phone-number']
+    t.pricing = row['pricing']
+    t.cuisine_type = row['cuisine type']
+    t.open = row['open']
+    t.close = row['close']
+    t.num_stars = row['num stars']
+    t.num_of_views = row['num_of_views']
+    t.save!
+    puts "#{t.name} saved"
+end
+puts "There are now #{Restaurant.count} rows in the Restaurant table"
+Restaurant.all.each_with_index do |restaurant, idx|
+    puts "#{idx} for #{restaurant.name}"
+    file = open("https://gimmeatable-dev.s3-us-west-1.amazonaws.com/res_#{idx}.jpg")
+    restaurant.photos.attach(io: file, filename: "res_#{idx}.jpg")
+    puts "#{restaurant.name} photos have been saved"
+end
+puts "All photos have been saved"
+
+another_guy = User.new(email:'anotherdemo@gmail.com', password: 'password', first_name: 'Guest', last_name:'User', city: 'Oakland')
+another_guy.save!
 # Location.first.photos.attach(io: open("https://gimmeatable-dev.s3-us-west-1.amazonaws.com/locshow_0.jpg"), filename: "locshow_0.jpg")
 # Location.all[1].photos.attach(io: open("https://gimmeatable-dev.s3-us-west-1.amazonaws.com/locshow_1.jpg"), filename: "locshow_1.jpg")
 # Location.all[2].photos.attach(io: open("https://gimmeatable-dev.s3-us-west-1.amazonaws.com/locshow_2.jpg"), filename: "locshow_2.jpg")
@@ -48,38 +83,6 @@ end
 # Location.all[7].photos.attach(io: open("https://gimmeatable-dev.s3-us-west-1.amazonaws.com/locshow_7.jpg"), filename: "locshow_7.jpg")
 # Location.all[8].photos.attach(io: open("https://gimmeatable-dev.s3-us-west-1.amazonaws.com/locshow_8.jpg"), filename: "locshow_8.jpg")
 # Location.all[9].photos.attach(io: open("https://gimmeatable-dev.s3-us-west-1.amazonaws.com/locshow_9.jpg"), filename: "locshow_9.jpg")
-# csv_text = File.read(Rails.root.join('db','lib','finalSeed.csv'))
-# csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
-# csv.each do |row|
-#     t = Restaurant.new
-
-#     t.name = row['name']
-#     t.description = row['description']
-#     t.hours_op = row['hours_op']
-#     t.dresscode = row['dresscode']
-#     t.address = row['address']
-#     t.neighborhood = row['neighborhood']
-#     t.website = row['website']
-#     t.city_id = row['city_id']
-#     t.phone_num= row['phone-number']
-#     t.pricing = row['pricing']
-#     t.cuisine_type = row['cuisine type']
-#     t.open = row['open']
-#     t.close = row['close']
-#     t.num_stars = row['num stars']
-#     t.num_of_views = row['num_of_views']
-#     t.save!
-#     puts "#{t.name} saved"
-# end
-# puts "There are now #{Restaurant.count} rows in the Restaurant table"
-# Restaurant.all.each_with_index do |restaurant, idx|
-#     puts "#{idx} for #{restaurant.name}"
-#     file = open("https://gimmeatable-dev.s3-us-west-1.amazonaws.com/res_#{idx}.jpg")
-#     restaurant.photos.attach(io: file, filename: "res_#{idx}.jpg")
-#     puts "#{restaurant.name} photos have been saved"
-# end
-# puts "All photos have been saved"
-
 # # first_res.save!
 # # p file
 # # first_res = Restaurant.create(name: 'Cafe Claudius', description:'Close to Union Square and closer to Paris, 
@@ -96,12 +99,9 @@ end
 
 
 
-# User.destroy_all
 
 # new_guy = User.new(email:'demouser@gmail.com', password: 'password', first_name: 'guest', last_name:'user', city: 'Oakland')
 # new_guy.save!
-# another_guy = User.new(email:'anotherdemo@gmail.com', password: 'password', first_name: 'Ariel', last_name:'Solomon', city: 'Oakland')
-# another_guy.save!
 
 # Restaurant.destroy_all
 
